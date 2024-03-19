304fc84ef161f8c4288e3054e064e194765ba41e
jay <155500056+Ragnarok344@users.noreply.github.com>
2024-03-19 00:20:18 -0400

    add extras

--- assets/scores.js
+++ assets/scores.js
@@ -1,18 +1,30 @@
 function printHighscores() {
-    // either get scores from localstorage or set to empty array
+    
     var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
   
-    // sort highscores by score property in descending order
+    
     highscores.sort(function (a, b) {
       return b.score - a.score;
     });
   
     for (var i = 0; i < highscores.length; i += 1) {
-      // create li tag for each high score
+      
       var liTag = document.createElement('li');
       liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+
+      if (i === 0) {
+        liTag.setAttribute('class', 'top-score');
+        liTag.style.color = 'red';
+        liTag.style.fontWeight = 'bold';
+
+        var imgTag = document.createElement('img');
+        imgTag.setAttribute('src', 'https://media.istockphoto.com/id/1168757141/vector/gold-trophy-with-the-name-plate-of-the-winner-of-the-competition.jpg?s=612x612&w=0&k=20&c=ljsP4p0yuJnh4f5jE2VwXfjs96CC0x4zj8CHUoMo39E=');
+        imgTag.style.width = '50px';
+
+        liTag.appendChild(imgTag);
+      }
   
-      // display on page
+      
       var olEl = document.getElementById('highscores');
       olEl.appendChild(liTag);
     }
@@ -25,6 +37,6 @@ function printHighscores() {
   
   document.getElementById('clear').onclick = clearHighscores;
   
-  // run function when page loads
+  
   printHighscores();
   
